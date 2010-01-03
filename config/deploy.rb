7335d09479f8a4d75944a6d1fbbda597e725183f
Vincent Landgraf <setcool@gmx.de>
2010-01-03 18:26:39 +0100

    changed deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,3 +1,5 @@
+default_run_options[:pty] = true
+
 set :application, "beermat"
 set :repository, "git://github.com/threez/beermat.git"
 set :scm, "git"
@@ -10,10 +12,16 @@ role :web, domain
 role :app, domain
 role :db,  domain, :primary => true
 
+after "deploy:symlink", "deploy:link_db"
+
 namespace :deploy do
   task(:start) {}
   task(:stop) {}
   task(:restart, :roles => :app, :except => { :no_release => true }) do
     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+  task(:link_db) do
+    run "rm -rf #{deploy_to}/current/db"
+    run "ln -s #{deploy_to}/shared/db #{deploy_to}/current/db"
   end
 end
